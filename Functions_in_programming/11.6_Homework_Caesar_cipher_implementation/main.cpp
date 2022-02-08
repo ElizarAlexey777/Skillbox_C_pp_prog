993c526d533fe772302420add4c75e2ed8e06e98
Elizarov Alexey Alexandrovich <73603842+ElizarAlexey777@users.noreply.github.com>
2022-02-08 20:26:59 +0300

    commit_08_02_2022_FuncInProg_CaesarCipher_2_Correction

--- Functions_in_programming/11.6_Homework_Caesar_cipher_implementation/main.cpp
+++ Functions_in_programming/11.6_Homework_Caesar_cipher_implementation/main.cpp
@@ -1,48 +1,5 @@
 #include <iostream>
 
-//std::string encrypt_caesar(std::string stroka, int num_sdvig) {
-//    std::string encr_stroka;
-//    std::string up_letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//    std::string down_letter = "abcdefghijklmnopqrstuvwxyz";
-//
-//    while (num_sdvig > 26) {num_sdvig -= 26;}
-//
-//    for (int i = 0; i < stroka.length(); i++) {
-//        if (down_letter.find(stroka[i]) != std::string::npos) {
-//            int index_let = down_letter.find(stroka[i]);
-//            int cur_stroka;
-//            int cur_num_sdvig;
-//            if (index_let + num_sdvig >= down_letter.length()) {
-//                cur_num_sdvig = num_sdvig - (down_letter.length() - index_let);
-//                cur_stroka = 0;
-//            } else {
-//                cur_num_sdvig = num_sdvig;
-//                cur_stroka = index_let;
-//            }
-//            encr_stroka += down_letter[cur_stroka + cur_num_sdvig];
-//        }
-//
-//        else if (up_letter.find(stroka[i]) != std::string::npos) {
-//            int index_let = up_letter.find(stroka[i]);
-//            int cur_stroka;
-//            int cur_num_sdvig;
-//            if (index_let + num_sdvig >= up_letter.length()) {
-//                cur_num_sdvig = num_sdvig - (up_letter.length() - index_let);
-//                cur_stroka = 0;
-//            } else {
-//                cur_num_sdvig = num_sdvig;
-//                cur_stroka = index_let;
-//            }
-//            encr_stroka += up_letter[cur_stroka + cur_num_sdvig];
-//        }
-//
-//        else {
-//            encr_stroka += stroka[i];
-//        }
-//    }
-//    return encr_stroka;
-//}
-
 std::string encrypt_caesar(std::string stroka, int num_sdvig) {
     std::string encr_stroka;
     encr_stroka = stroka;
@@ -76,7 +33,7 @@ std::string encrypt_caesar(std::string stroka, int num_sdvig) {
 
 
 std::string decrypt_caesar(std::string stroka, int num_sdvig) {
-    stroka = encrypt_caesar(stroka, num_sdvig);
+    stroka = encrypt_caesar(stroka, -num_sdvig);
     return stroka;
 }
 
@@ -103,7 +60,7 @@ int main() {
     std::cout << std::endl;
 
     std::cout << "Reverse transformation: from the Caesar cipher to the original string:" << std::endl;
-    std::cout << decrypt_caesar(encrypt_caesar(stroka, num_sdvig), -num_sdvig) << std::endl;
+    std::cout << decrypt_caesar(encrypt_caesar(stroka, num_sdvig), num_sdvig) << std::endl;
     std::cout << std::endl;
     return 0;
 }
