df70a1dc5053b06b38a01c0c3761058cdf1bb0c0
Elizarov Alexey Alexandrovich <73603842+ElizarAlexey777@users.noreply.github.com>
2022-02-21 08:57:00 +0300

    commit_21_02_22_Arrays_ApartHouse_2

--- One_dimensional_arrays_Basic_algorithms_on_arrays/12.6_Homework_Apartment_house/main.cpp
+++ One_dimensional_arrays_Basic_algorithms_on_arrays/12.6_Homework_Apartment_house/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Apartment house" << std::endl;
+    std::cout << std::endl;
+
+    std::string tenants[10];
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Enter the last name of " << i + 1 << " tenant:" << std::endl;
+        std::cin >> tenants[i];
+    }
+
+    int apart_num;
+    std::cout << "Enter three apartment numbers:" << std::endl;
+    for (int i = 0; i < 3; i++) {
+        std::cin >> apart_num;
+        if (apart_num > 10 or apart_num < 1) {
+            std::cout << "Invalid apartment number" << std::endl;
+            continue;
+        }
+        std::cout << tenants[apart_num-1] << " lives in apartment number " << apart_num << std::endl;
+    }
     return 0;
 }
