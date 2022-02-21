73ad01a94b984e00e69286b2b3528680c62ae4a2
Elizarov Alexey Alexandrovich <73603842+ElizarAlexey777@users.noreply.github.com>
2022-02-21 12:16:42 +0300

    commit_21_02_22_Arrays_Row_of_numbers_2

--- One_dimensional_arrays_Basic_algorithms_on_arrays/12.6_Homework_Row_of_numbers/main.cpp
+++ One_dimensional_arrays_Basic_algorithms_on_arrays/12.6_Homework_Row_of_numbers/main.cpp
@@ -6,20 +6,32 @@ int main() {
 
     int numbers[15] = {114,111, 106, 107, 108, 105, 115,  108, 110, 109, 112, 113, 116, 117, 118};
 
-    for (int number : numbers) {
-        int count = 0;
-        for (int j : numbers) {
-            if (number == j) {
-                count++;
-                if (count >= 2) {
-                    std::cout << "Repeating number: " << number << std::endl;
-                    break;
-                }
-            }
-        }
-        if (count >= 2) {
-            break;
-        }
+    int min = numbers[0];
+    for (int number : numbers) if (number < min) min = number;
+
+    int summa = 0;
+    for (int number : numbers) summa = summa + number;
+
+    for (int i = 0; i < 14; i++) {
+        summa -= min;
+        min += 1;
     }
+    std::cout << "Repeating number: " << summa << std::endl;
+
+//    for (int number : numbers) {
+//        int count = 0;
+//        for (int j : numbers) {
+//            if (number == j) {
+//                count++;
+//                if (count >= 2) {
+//                    std::cout << "Repeating number: " << number << std::endl;
+//                    break;
+//                }
+//            }
+//        }
+//        if (count >= 2) {
+//            break;
+//        }
+//    }
     return 0;
 }
