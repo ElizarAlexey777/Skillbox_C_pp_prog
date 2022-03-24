11766a8c4e77478899445f8c65f0d0cb1a717f07
Elizarov Alexey Alexandrovich <73603842+ElizarAlexey777@users.noreply.github.com>
2022-03-24 22:06:46 +0300

    commit_24_03_22_Two_dim_arrays_Multi_matrix_2

--- Two_dimensional_arrays_and_algorithms/Lesson_3_Matrix/14.6_Homework_Multi_matrix_vector/main.cpp
+++ Two_dimensional_arrays_and_algorithms/Lesson_3_Matrix/14.6_Homework_Multi_matrix_vector/main.cpp
@@ -6,27 +6,43 @@ int main() {
     std::cout << std::endl;
 
     std::vector<float> V(4);
-    float matrix[4][4];
+    float M[4][4];
 
-    std::cout << "Enter vector elements: " << std::endl;
+    std::cout << "Enter vector V elements: " << std::endl;
     for (int i = 0; i < 4; i++) std::cin >> V[i];
 
-    std::cout << "Enter matrix elements: " << std::endl;
-    for (auto & i : matrix) {
+    std::cout << "Enter matrix M elements: " << std::endl;
+    for (auto & i : M) {
         for (float & j : i) {
             std::cin >> j;
         }
     }
 
+    std::cout << std::endl;
+    std::cout << "Vector V: " << std::endl;
+    for (float i : V) {
+        std::cout << i << ' ';
+    } std::cout << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Matrix M: " << std::endl;
+    for (auto & i : M) {
+        for (float & j : i) {
+            std::cout << j << ' ';
+        }
+        std::cout << std::endl;
+    } std::cout << std::endl;
+
     float R[4];
     for (int i = 0; i < 4; i++) {
         float summa = 0.f;
         for (int j = 0; j < 4; j++) {
-            summa += matrix[i][j] * V[i];
+            summa += M[i][j] * V[i];
         }
         R[i] = summa;
     }
 
+    std::cout << "Array R: " << std::endl;
     for (float i : R) {
         std::cout << i << ' ';
     } std::cout << std::endl;
