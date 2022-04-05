95980535f9fbb4670c1d963a424b72111ecb0b51
Elizarov Alexey Alexandrovich <73603842+ElizarAlexey777@users.noreply.github.com>
2022-04-05 21:40:58 +0300

    commit_05_03_22_Alg_on_arrays_Task_1_2

--- Algorithms_on_arrays/Task_1/main.cpp
+++ Algorithms_on_arrays/Task_1/main.cpp
@@ -9,20 +9,39 @@ int main() {
     std::vector<int> vec = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     int i = 0;
     int j = 0;
-    int summa_max = 0;
-    int summa_temp;
+    int min_summa = 0;
+    int min_pos = -1;
+    int summa = 0;
+    int answer = vec[0];
 
     for (int x = 0; x < vec.size(); x++) {
-        summa_temp = vec[x];
-        for (int y = x + 1; y < vec.size(); y++) {
-            summa_temp += vec[y];
-            if (summa_temp > summa_max) {
-                i = x;
-                j = y;
-                summa_max = summa_temp;
-            }
+        summa += vec[x];
+        int summa_temp = summa - min_summa;
+
+        if (summa_temp > answer) {
+            answer = summa_temp;
+            i = min_pos + 1;
+            j = x;
+        }
+
+        if (summa < min_summa) {
+            min_summa = summa;
+            min_pos = x;
         }
     }
+
+//    int summa_max = 0;
+//    for (int x = 0; x < vec.size(); x++) {
+//        summa_temp = vec[x];
+//        for (int y = x + 1; y < vec.size(); y++) {
+//            summa_temp += vec[y];
+//            if (summa_temp > summa_max) {
+//                i = x;
+//                j = y;
+//                summa_max = summa_temp;
+//            }
+//        }
+//    }
     std::cout << "Range frames: " << i << " " << j << std::endl;
     return 0;
 }
