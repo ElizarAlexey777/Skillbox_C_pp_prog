a3ef8b6c03aa8e96fe47351fe892dbaae1e1957f
Elizarov Alexey Alexandrovich <73603842+ElizarAlexey777@users.noreply.github.com>
2022-04-19 22:53:28 +0300

    commit_19_04_22_Alg_on_arrays_Task_4_2

--- Algorithms_on_arrays/Task_4/main.cpp
+++ Algorithms_on_arrays/Task_4/main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> sort_vec_abs(std::vector<int> vec) {
-    for (int i = 0; i < vec.size(); i++) {
-        for (int j = 0; j < vec.size() - i - 1; j++) {
-            if (std::abs(vec[j]) > std::abs(vec[j+1])) {
-                std::swap(vec[j], vec[j+1]);
+void sort_vec_abs(std::vector<int> vec) {
+    bool flag = false;
+    int count_sdvig = 0;
+    int ind_first_pol = 0;
+    for (int i = 0; i + count_sdvig < vec.size(); i++) {
+        if (vec[i] >= 0 and !flag) {
+            flag = true;
+            ind_first_pol = i;
+            std::cout << vec[i] << ' ';
+        }
+        if (flag) {
+            count_sdvig += 1;
+            if (std::abs(vec[ind_first_pol + count_sdvig]) > std::abs(vec[ind_first_pol - count_sdvig])) {
+                if (ind_first_pol - count_sdvig >= 0) {
+                    std::cout << vec[ind_first_pol - count_sdvig] << ' ';
+                }
+                if (ind_first_pol + count_sdvig < vec.size()) {
+                    std::cout << vec[ind_first_pol + count_sdvig] << ' ';
+                }
+            } else {
+                if (ind_first_pol + count_sdvig < vec.size()) {
+                    std::cout << vec[ind_first_pol + count_sdvig] << ' ';
+                }
+                if (ind_first_pol - count_sdvig >= 0) {
+                    std::cout << vec[ind_first_pol - count_sdvig] << ' ';
+                }
             }
+            i -= 1;
         }
     }
-    return vec;
 }
 
 int main() {
@@ -19,8 +40,7 @@ int main() {
     std::vector<int> vec = {-100, -50, -5, 1, 10, 15};
 
     std::cout << "Array sorted in ascending modulus: " << std::endl;
-    for (int i : sort_vec_abs(vec)) {
-        std::cout << i << ' ';
-    } std::cout << std::endl;
+    sort_vec_abs(vec);
+    std::cout << std::endl;
     return 0;
 }
