35b2568e8d7f7aff1bb6d0c5f96e34a087cc3a79
Elizarov Alexey Alexandrovich <73603842+ElizarAlexey777@users.noreply.github.com>
2022-06-07 19:09:17 +0300

    commit_07_06_22_pointers_task3_1

---
+++ Pointers/17.4_Homework_Task3/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+bool substr(char* a, char* b) {
+    int i = 0;
+    bool res;
+    while (*(a+i) != '\0') {
+        if (*(b) == *(a+i)) {
+            int j = 0;
+            res = true;
+            while (*(b+j) != '\0') {
+                if (*(b + j) == *(a + i)) {
+                    i++;
+                    j++;
+                } else {
+                    i += j;
+                    res = false;
+                    break;
+                }
+            }
+        } else i++;
+    }
+    return res;
+}
+
+int main() {
+    std::cout << "Task 3" << std::endl;
+    std::cout << std::endl;
+
+    char* a = "Hello world";
+
+    char* b = "wor";
+
+    char* c = "banana";
+
+    std::cout << substr(a,b) << " " << substr(a,c);
+    return 0;
+}
