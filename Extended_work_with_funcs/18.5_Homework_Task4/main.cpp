8dc88442c4df237f1f6ad4b44da37723ad612d61
Elizarov Alexey Alexandrovich <73603842+ElizarAlexey777@users.noreply.github.com>
2022-06-10 12:21:43 +0300

    commit_10_06_22_ext_work_func_task4_1

---
+++ Extended_work_with_funcs/18.5_Homework_Task4/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void evendigits(long long number, int& ans) {
+    if (number != 0) {
+        if (number % 2 == 0) ans += 1;
+        evendigits(number / 10, ans);
+    }
+}
+
+int main() {
+    std::cout << "Task 4" << std::endl;
+    std::cout << std::endl;
+
+    long long number;
+    int ans;
+
+    std::cout << "Enter a long-long number: " << std::endl;
+    std::cin >> number;
+
+    evendigits(number, ans);
+
+    std::cout << "The number of even digits in the long-long number: " << ans << std::endl;
+
+    return 0;
+}
