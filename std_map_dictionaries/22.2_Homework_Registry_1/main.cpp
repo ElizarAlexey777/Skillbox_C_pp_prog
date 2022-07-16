e1914c34ce13019a8231622f07d20d03a59d141e
Elizarov Alexey Alexandrovich <73603842+ElizarAlexey777@users.noreply.github.com>
2022-07-16 20:59:25 +0300

    commit_16_07_22_map_dicts_Registry_2

--- std_map_dictionaries/22.2_Homework_Registry_1/main.cpp
+++ std_map_dictionaries/22.2_Homework_Registry_1/main.cpp
@@ -2,12 +2,22 @@
 #include <map>
 #include <string>
 
+std::string key_to_name(std::string key) {
+    for (int ind_ = 0; ind_ < key.size(); ind_++) {
+        if (key[ind_] == '_') {
+            key.erase(ind_);
+            break;
+        }
+    }
+    return key;
+}
+
 int main() {
     std::cout << "Registry" << std::endl;
     std::cout << std::endl;
 
     int num_client = 0;
-    std::map<std::string, std::string> registry;
+    std::map<std::string, int> registry;
     std::string input;
     std::string key_input;
 
@@ -22,13 +32,13 @@ int main() {
 
         if (input == "Next") {
             if (num_client > 0) {
-                std::cout << "Next client in the queue: " << registry.begin()->second << std::endl;
+                std::cout << "Next client in the queue: " << key_to_name(registry.begin()->first) << std::endl;
                 registry.erase(registry.begin());
                 num_client--;
             } else std::cout << "Queue is empty :(" << std::endl;
         } else {
-            key_input = input + std::to_string(num_client);
-            registry[key_input] = input;
+            key_input = input + "_" + std::to_string(num_client);
+            registry[key_input] = num_client;
             num_client++;
         }
         std::cout << std::endl;
