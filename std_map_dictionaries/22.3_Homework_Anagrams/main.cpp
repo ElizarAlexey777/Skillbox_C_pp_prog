c9f411154156565659126b20880185d2aaaaf667
Elizarov Alexey Alexandrovich <73603842+ElizarAlexey777@users.noreply.github.com>
2022-07-16 23:08:12 +0300

    commit_16_07_22_map_dics_Anagrams_2

--- std_map_dictionaries/22.3_Homework_Anagrams/main.cpp
+++ std_map_dictionaries/22.3_Homework_Anagrams/main.cpp
@@ -2,29 +2,38 @@
 #include <string>
 #include <map>
 
-bool check_anagram(std::string & first, std::string & second) {
-    std::map <char, int> anagram;
+//bool check_anagram(std::string & first, std::string & second) {
+//    std::map <char, int> anagram;
+//
+//    if (first.length() == second.length()) {
+//        for (int j = 0; j < first.length(); j++) {
+//            if (anagram.find(first[j]) != anagram.cend()) {
+//                auto it = anagram.find(first[j]);
+//                it-> second++;
+//            } else anagram.insert(std::pair<char, int> (first[j], 1));
+//        }
+//        for (int j = 0; j < second.length(); j++) {
+//            if (anagram.find(second[j]) != anagram.cend()) {
+//                auto it = anagram.find(second[j]);
+//                it->second--;
+//            } else return false;
+//        }
+//        for (auto j = anagram.begin(); j != anagram.end(); j++) {
+//            if (j->second != 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    } else return false;
+//}
 
-    if (first.length() == second.length()) {
-        for (int j = 0; j < first.length(); j++) {
-            if (anagram.find(first[j]) != anagram.cend()) {
-                auto it = anagram.find(first[j]);
-                it-> second++;
-            } else anagram.insert(std::pair<char, int> (first[j], 1));
-        }
-        for (int j = 0; j < second.length(); j++) {
-            if (anagram.find(second[j]) != anagram.cend()) {
-                auto it = anagram.find(second[j]);
-                it->second--;
-            } else return false;
-        }
-        for (auto j = anagram.begin(); j != anagram.end(); j++) {
-            if (j->second != 0) {
-                return false;
-            }
-        }
-        return true;
-    } else return false;
+std::map<char, int> countChars(std::string& string) {
+    std::map<char, int> result;
+    for (int i = 0; i < string.length(); ++i) {
+        std::pair<char,int> pair = {string[i],string[i]};
+        result.insert(pair);
+    }
+    return result;
 }
 
 int main() {
@@ -34,12 +43,13 @@ int main() {
 
     std::cout << "Enter the first word:" << std::endl;
     std::cin >> first;
+    std::map<char,int> first_m = countChars(first);
+
     std::cout << "Enter the second word:" << std::endl;
     std::cin >> second;
+    std::map<char,int> second_m = countChars(second);
 
-    bool result = check_anagram(first, second);
-
-    if (result) {
+    if (first_m == second_m) {
         std::cout << "The first line is an anagram of the second line" << std::endl;
     } else {
         std::cout << "The first line is not an anagram of the second line" << std::endl;
