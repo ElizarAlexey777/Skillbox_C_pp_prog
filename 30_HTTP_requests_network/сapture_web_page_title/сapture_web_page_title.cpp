2f19f2fc7f7fff352ec222451e2c9cdcf26d05d7
Elizarov Alexey <73603842+ElizarAlexey777@users.noreply.github.com>
2024-08-11 16:09:24 +0300

    feat: add task_3 to 30_http

---
+++ 30_HTTP_requests_network/сapture_web_page_title/сapture_web_page_title.cpp
@@ -0,0 +1,25 @@
+﻿#include <cpr/cpr.h>
+#include <iostream>
+#include <string>
+
+int main() {
+    cpr::Response response = cpr::Get(cpr::Url{ "http://httpbin.org/html" },
+        cpr::Header{ {"accept", "text/html"} });
+
+    if (response.status_code == 200) {
+        std::string::size_type start_pos = response.text.find("<h1>");
+        std::string::size_type end_pos = response.text.find("</h1>");
+
+        if (start_pos != std::string::npos && end_pos != std::string::npos) {
+            std::string title = response.text.substr(start_pos + 4, end_pos - start_pos - 4);
+
+            std::cout << "Заголовок статьи: " << title << std::endl;
+        } else {
+            std::cout << "Заголовок статьи не найден." << std::endl;
+        }
+    } else {
+        std::cout << "Запрос не удался, статус код: " << response.status_code << std::endl;
+    }
+
+    return 0;
+}
---
+++ 30_HTTP_requests_network/сapture_web_page_title/сapture_web_page_title.h
@@ -0,0 +1,8 @@
+﻿// сapture_web_page_title.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
